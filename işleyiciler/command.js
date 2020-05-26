8c5ed87bb2dbf7edf444d07c3cf07cfc9764d8b3
Glitch (hello-express) <none>
2020-05-26 13:13:08 +0000

    👫🎹 Checkpoint ./işleyiciler/command.js:11150964/113

--- işleyiciler/command.js
+++ işleyiciler/command.js
@@ -2,12 +2,12 @@ const { readdirSync } = require('fs');
 const ascii = require('ascii-table'); // npm install ascii-table 
 // ASCİİ şeklinde komut yükleyicisidir, bir hata olsa bile komutları yüklemeye devam eder. TheSourceCode'den alıntıdır, bizde yalan yok :D.
 
-let table = new ascii("Commands");
-table.setHeading('Command', 'Load status');
+let table = new ascii("Komutlar");
+table.setHeading('Komut', 'Durum'); //Türkçeye çevrildi 
 
 
 module.exports  = (client) => {
-    readdirSync("./komutlar/").forEach(dir => {
+    readdirSync("./komutlar/").forEach(dir => { //Komutlar klasörünü okumayı sağlar
         const commands = readdirSync(`./komutlar/${dir}/`).filter(file => file.endsWith('.js')); //.JS Uzantısıyla biten dosyaları botun okumasını sağlar.
         for(let file of commands) { //Hatasız komutları yükler
             let pull = require(`../komutlar/${dir}/${file}`);
