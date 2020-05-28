a4d53913aa6d28af73e6513886620fa316cf65d7
Glitch (hello-express) <none>
2020-05-28 13:40:56 +0000

    🏍📼 Checkpoint ./komutlar/test.js:16368977/756

--- komutlar/test.js
+++ komutlar/test.js
@@ -4,14 +4,20 @@ module.exports= {
   name: 'test',
   description: 'test',
   run: async(client, message, args) => {
+
     
-const embed = new discord.MessageEmbed()
-.setAuthor(client.user.username, client.user.avatarURL())
+const embed = new discord.RichEmbed()
+.setAuthor(client.user.username, client.user.avatarURL)
 .setTitle('Deneme')
 .setColor('RANDOM')
 .setDescription('deneme')
 .setFooter('Deneme')
-.setImage('')
+.setImage('https://cdn.discordapp.com/attachments/715553636259987586/715556213747613786/download.jpg')
+.setThumbnail(client.user.avatarURL)
+.setTimestamp()
+.addField('deneme', 'test', true)
+.addField('deneme', 'test', true)
+.addField('deneme', 'test', true)
 message.channel.send(embed)
 
   }
