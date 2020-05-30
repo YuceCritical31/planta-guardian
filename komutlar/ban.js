0ec526e70f90bc0d57cf7fc9ad18e70242bf3c84
Glitch (hello-express) <none>
2020-05-30 14:04:19 +0000

    🗺🏠 Checkpoint ./komutlar/ban.js:16368977/1607

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -11,8 +11,35 @@ let kanal = db.fetch(`banlog_${message.guild.id}`)
 if(!message.member.roles.cache.has(yetkili)) return message.channel.send(`Bu Komudu Kullanabilmen İçin <@&${yetkili}> Rolüne Sahip Olmalısın ! `)
 
   const kisi = message.mentions.users.first()
-let reason = args.slice(1).join(' ');
+let reason = args.slice(1).join(' ')
+if (!kisi) return message.channel.send(`Banlayacağın Kullanıcıyı Belirtmelisin ! `)
+if (!reason) return message.channel.send(`Banlama Sebebini Belirtmelisin ! `)
 
+const embed = new discord.MessageEmbed()
+.setAuthor(client.user.username, client.user.avatarURL())
+.setTitle(`${client.user.username} - ban`)
+.setColor('BLACK')
+.setDescription(`${kisi} Adlı Kullanıcı **${reason}** Sebebiyle Banladı ! `)
+.setThumbnail(client.user.avatarURL())
+.setFooter(`Komut ${message.author.tag} Tarafından Kullanıldı ! `)
+message.channel.send(embed)
+
+kisi.send(`**${message.guild.name}** Adlı Sunucudan **${reason}** Sebebiyle Banlandın ! `)
+
+message.guild.members.ban(kisi, {
+  reason: reason
+})
+
+ let bantasi = db.get(`banlog_${message.guild.id}`)
+  const banka = client.channels.get(bantasi)
+banka.send(
+new discord.MessageEmbed()
+.setAuthor(client.user.username, client.user.avatarURL())
+.setTitle(`${client.user.username} - Ban`)
+.setColor('BLACK')
+.setDescription(`Banlanan Kullanıcı: ${kisi} \n Banlayan Yetkili: <@!${message.author.id}> \n Banlanma Sebebi: **${reason}**`)
+.setThumbnail(client.user.avatarURL())
+)
 
 }
 exports.conf = {
