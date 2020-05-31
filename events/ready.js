22f75c81086801055c41516787066e61f62457fc
Glitch (hello-express) <none>
2020-05-31 12:23:08 +0000

    👆👮 Checkpoint ./komutlar/kız.js:16368977/17 ./komutlar/erkek.js:16368977/21 ./events/ready.js:16368977/39

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const Discord = require('discord.js');
 const prefix = process.env.PREFIX;
 
 module.exports = client => {
-client.user.setActivity('Deneme', {type: 'PLAYING'}); 
+client.user.setActivity('Sunucudaki Üyeleri', {type: 'WATCHING'}); 
 
 }
