c0b998d630920dc23c62c3597a22870e06b3966e
Glitch (hello-express) <none>
2020-10-11 06:46:58 +0000

    🐀😚 Checkpoint ./komutlar/erkek.js:23648482/1472 ./komutlar/kız.js:23648482/4750

--- komutlar/kız.js
+++ komutlar/kız.js
@@ -6,43 +6,38 @@ const db = require('quick.db')
 exports.run = async(client, message, args) => {
 
 let kanal = db.fetch(`kayıtkanal_${message.guild.id}`)
-let alınacakrol = db.fetch(`alınacakrol_${message.guild.id}`)
-let kızrol = db.fetch(`kızrol_${message.guild.id}`)
-let kayıtçı = db.fetch(`kayıtçırol_${message.guild.id}`)
 let kayıtsayı = db.fetch(`kayıtsayı_${message.author.id}`)
   
-if(!message.member.roles.cache.has(kayıtçı)) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> Bu Komudu Kullanabilmen İçin <@&${kayıtçı}> Adlı Role Sahip olman Lazım ! `))
+if(!message.member.roles.has('755779189969256479')) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> Bu Komudu Kullanabilmen İçin <@755779189969256479> Adlı Role Sahip olman Lazım ! `))
 if(message.channel.id !== kanal) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> Bu Komudu Sadece <#${kanal}> Adlı Kanalda Kullanabilirsin ! `))
-if (!kızrol) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> Sunucuda Kız Rolü Ayarlanmadığı İçin Komut Kullanılamaz ! `))
 
 let member = message.mentions.members.first();
-if (!member) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> Kız Olarak Kayıt Edeceğin Kullanıcıyı Belirtmelisin ! `))
+if (!member) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> Erkek Olarak Kayıt Edeceğin Kullanıcıyı Belirtmelisin ! `))
 let isim = args[1]
 if (!isim) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> İsmini Belirtmelisin ! `))
 let yaş = args[2]
 if (!yaş) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> Yaşını Belirtmelisin ! `))
-member.setNickname(`ꖑ ${isim} | ${yaş}`)
-member.roles.remove(alınacakrol)
-member.roles.add(kızrol) 
+member.setNickname(`乡 ${isim} | ${yaş}`)
+member.roles.remove('755779192393564240')
+member.roles.add('755779188753039471')
 
-const başarılı = new discord.MessageEmbed()
-.setColor('PURPLE')
-.setDescription(`<a:sagok:757855573554233396> Kız Olarak Kayıt Edilen Kullanıcı: ${member} \n\n <a:sagok:757855573554233396> Kız Olarak Kayıt Eden Yetkili: <@!${message.author.id}> \n\n <a:sagok:757855573554233396> Kız Olarak Kayıt Eden Kullanıcının Kayıt Sayısı: **${kayıtsayı ? `**${kayıtsayı}**` : "0"}**`)
-.addField(`Kullanıcının ismi;`, `${isim}`, true)
-.addField(`Kullanıcının Yaşı;`, `${yaş}`, true)
+const darkcode = new discord.MessageEmbed()
+.setColor('BLUE')
+.setDescription(` ${member} **Adlı üyeye** <@755779188753039471> rolünü verip ismini \`乡 ${isim} | ${yaş}\` **olarak ayarladım**
+                 \n <a:sagok:757855573554233396> Erkek Olarak Kayıt Eden Kullanıcının Kayıt Sayısı: **${kayıtsayı ? `${kayıtsayı}` : "0"}**`)
 .setThumbnail(member.avatarURL)
 .setFooter(`Developer by qmi `)
-message.channel.send(başarılı)
+message.channel.send(darkcode)
 db.add(`kayıtsayı_${message.author.id}`, 1)
 }
 exports.conf = {
   enabled: true,
   guildonly: false,
-  aliases: ['k'],
+  aliases: ['e'],
   permlevel: 0
 }
 exports.help = {
-  name: 'kız',
+  name: 'erkek',
   description: 'erkek olarak kayıt eder',
   usage: '!erkek @kullanıcı isim yaş'
 }
