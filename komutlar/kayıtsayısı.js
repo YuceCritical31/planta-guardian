f762c4fc15aa1dd8b1e8399a61bd27176548a4bd
Glitch (hello-express) <none>
2020-10-13 16:44:05 +0000

    🍮🎤 Checkpoint ./komutlar/kayıtsayısı.js:23648482/1448

--- komutlar/kayıtsayısı.js
+++ komutlar/kayıtsayısı.js
@@ -2,8 +2,9 @@ const Discord = require("discord.js");
 const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
-  let kayıtsayı = db.fetch(`kayıtsayı_${message.author.id}`)
+  let kayıtsayı = db.fetch(`kayıtsayı_${message.guild.id}`)
   let user = message.mentions.users.first();
+
     if(!user) return message.channel.send(new Discord.MessageEmbed().setDescription('<a:nono:751840894868521010> Davet sayısını görmek istediğiniz yetkiyi etiketlemelisin'));
 
 
@@ -11,6 +12,7 @@ exports.run = async (client, message, args) => {
 
 message.channel.send(new Discord.MessageEmbed()
 .setDescription(`Etiketlediğiniz Kullanıcının Toplam Kayıt Sayısı: **${kayıtsayı ? `**${kayıtsayı}**` : "0"}**`))
+.setThumbnail(user.avatarURL)
 .setColor('BLUE')
 .setFooter(`Komut ${message.author.tag} Tarafından Kullanıldı ! `)
 
