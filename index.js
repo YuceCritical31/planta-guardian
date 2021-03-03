b68a15d20685fe44ecc4270911d46dd21f01121a
Glitch (v12guard-acebots) <none>
2021-03-03 21:21:16 +0000

    🍐🏫 Checkpoint ./index.js:26490528/62 ./ayarlar.json:26490528/37

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ setInterval(() => {
 /////////////////////////////////////////////ELLEME///////////////////////////////////////////
 function guvenli(kisiID) {
   let uye = client.guilds.cache.get(k.guildID).members.cache.get(kisiID);
-  let guvenliler = ayarlar.whitelist || ["451677789939171339","448377317065097228","776837094962102282"]; if (!uye || uye.id === client.user.id || uye.id === ayarlar.owner  || uye.id === uye.guild.owner.id || guvenliler.some(g => uye.id === g.slice(1) || uye.roles.cache.has(g.slice(1)))) return true
+  let guvenliler = ayarlar.whitelist || []; if (!uye || uye.id === client.user.id || uye.id === ayarlar.owner  || uye.id === uye.guild.owner.id || guvenliler.some(g => uye.id === g.slice(1) || uye.roles.cache.has(g.slice(1)))) return true
   else return false;
 };
 
