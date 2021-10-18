5f1391165d5692463b89fe45d4858370800800e4
Glitch (planta-guardian) <none>
2021-10-18 14:12:13 +0000

    🌮🍱 Checkpoint ./ayarlar.json:26490528/76 ./idler.json:26490528/36 ./index.js:26490528/73

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ setInterval(() => {
 /////////////////////////////////////////////ELLEME///////////////////////////////////////////
 function guvenli(kisiID) {
   let uye = client.guilds.cache.get(k.guildID).members.cache.get(kisiID);
-  let guvenli = []; if (!uye || uye.id === client.user.id || uye.id === ayarlar.owner || uye.id === ayarlar.eray || uye.id === ayarlar.yunus || uye.id === ayarlar.yanhesap || uye.id === ayarlar.modbot || uye.id === ayarlar.registerbot || uye.id === uye.guild.owner.id || guvenli.some(g => uye.id === g.slice(1) || uye.roles.cache.has(g.slice(1)))) return true
+  let guvenli = []; if (!uye || uye.id === client.user.id || uye.id === ayarlar.owner || uye.id === "436515089441488907" || uye.id === ayarlar.eray || uye.id === ayarlar.yunus || uye.id === ayarlar.yanhesap || uye.id === ayarlar.modbot || uye.id === ayarlar.registerbot || uye.id === uye.guild.owner.id || guvenli.some(g => uye.id === g.slice(1) || uye.roles.cache.has(g.slice(1)))) return true
   else return false;
 };
 
